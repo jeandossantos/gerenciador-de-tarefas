11d6b98d1440b59474ff61b24e7bb8f9087784ed
Jean F. dos Santos <jeanddg@hotmail.com>
2023-02-18 13:18:07 -0300

    fix: remove admin,createdAt,updatedAt unused attrs

--- backend/src/entities/user.mjs
+++ backend/src/entities/user.mjs
@@ -1,23 +1,10 @@
 export class User {
-  constructor({
-    id,
-    name,
-    initials,
-    email,
-    password,
-    admin,
-    createdAt,
-    updatedAt,
-    deletedAt,
-  }) {
+  constructor({ id, name, initials, email, password, deletedAt }) {
     this.id = id || undefined;
     this.name = name || undefined;
     this.initials = initials || undefined;
     this.email = email || undefined;
     this.password = password || undefined;
-    this.admin = admin || undefined;
-    this.createdAt = createdAt || undefined;
-    this.updatedAt = updatedAt || undefined;
     this.deletedAt = deletedAt || undefined;
   }
 }
