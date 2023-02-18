c279bc70878318947d84728f9cc31112801dea38
Jean F. dos Santos <jeanddg@hotmail.com>
2023-02-18 13:50:01 -0300

    fix: add needed confirmPassword attribute

--- backend/src/entities/user.mjs
+++ backend/src/entities/user.mjs
@@ -1,10 +1,19 @@
 export class User {
-  constructor({ id, name, initials, email, password, deletedAt }) {
+  constructor({
+    id,
+    name,
+    initials,
+    email,
+    password,
+    confirmPassword,
+    deletedAt,
+  }) {
     this.id = id || undefined;
     this.name = name || undefined;
     this.initials = initials || undefined;
     this.email = email || undefined;
     this.password = password || undefined;
+    this.confirmPassword = confirmPassword || undefined;
     this.deletedAt = deletedAt || undefined;
   }
 }
