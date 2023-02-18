42384021487f43d4d04b50ec9dabbcf90b76bf71
Jean F. dos Santos <jeanddg@hotmail.com>
2023-02-18 09:43:24 -0300

    feat: create validation.js

---
+++ backend/src/validation.mjs
@@ -0,0 +1,20 @@
+function existsOrError(value, msg, ClassError) {
+  if (!value) throw new ClassError(msg);
+  if (Array.isArray(value) && value.length === 0) throw new ClassError(msg);
+  if (typeof value === 'string' && !value.trim()) throw new ClassError(msg);
+}
+
+function notExistsOrError(value, msg) {
+  try {
+    existsOrError(value, msg);
+  } catch (msg) {
+    return;
+  }
+  throw new ClassError(msg);
+}
+
+function equalsOrError(valueA, valueB, msg) {
+  if (valueA !== valueB) throw new ClassError(msg);
+}
+
+export { existsOrError, notExistsOrError, equalsOrError };
