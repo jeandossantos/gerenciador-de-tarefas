d0534dc2a30976347d175f779a1e0551d1c185d7
Jean F. dos Santos <jeanddg@hotmail.com>
2023-02-20 16:35:51 -0300

    feat:  add TaskDataBuilder class

--- backend/tests/model/task/taskDataBuilder.mjs
+++ backend/tests/model/task/taskDataBuilder.mjs
@@ -3,16 +3,54 @@ import { Task } from '../../../src/entities/task.mjs';
 export class TaskDataBuilder {
   constructor() {
     this.tarefaData = {
+      id: 1,
       name: 'Primeiro Login',
       description: 'Está é uma tarefa criada por padrão pelo sistema.',
       priority: 0,
       deadline: new Date(),
-      done: true,
+      done: false,
+      userId: 123,
     };
   }
 
   static validTask() {
-    return new TaskDataBuilder().build();
+    return new TaskDataBuilder();
+  }
+
+  withInvalidName() {
+    this.tarefaData.name = '';
+
+    return this;
+  }
+
+  withInvalidUserId() {
+    this.tarefaData.userId = 'invalid-user-id';
+
+    return this;
+  }
+
+  withoutDescription() {
+    Reflect.deleteProperty(this.tarefaData, 'description');
+
+    return this;
+  }
+
+  withoutPriority() {
+    Reflect.deleteProperty(this.tarefaData, 'priority');
+
+    return this;
+  }
+
+  withoutDone() {
+    Reflect.deleteProperty(this.tarefaData, 'done');
+
+    return this;
+  }
+
+  withoutDeadline() {
+    Reflect.deleteProperty(this.tarefaData, 'deadline');
+
+    return this;
   }
 
   build() {
