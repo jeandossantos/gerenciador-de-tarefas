7cb1918904bf5f99d950177820fa05a12543c2b5
Jean F. dos Santos <jeanddg@hotmail.com>
2023-02-20 16:38:32 -0300

    feat:  add CreateTaskService class

---
+++ backend/src/service/task/createTaskService.mjs
@@ -0,0 +1,29 @@
+import { TaskError } from '../../errors/taskError.mjs';
+
+import { existsOrError } from '../../validation.mjs';
+
+function createTaskServiceValidation(task) {
+  existsOrError(task.name, 'Nome invalido!', TaskError);
+  existsOrError(task.userId, 'ID do usuário invalido!', TaskError);
+
+  if (isNaN(task.userId)) throw new TaskError('ID do usuário invalido!');
+}
+
+export class CreateTaskService {
+  constructor({ taskRepository }) {
+    this.taskRepository = taskRepository;
+  }
+
+  async execute({ name, description, priority, done, deadline, userId }) {
+    createTaskServiceValidation({ name, userId });
+
+    await this.taskRepository.create({
+      name,
+      description,
+      priority,
+      deadline,
+      done,
+      userId,
+    });
+  }
+}
