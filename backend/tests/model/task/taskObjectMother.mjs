91b6f91b3aa898feaced3446ec4f0751d30deee8
Jean F. dos Santos <jeanddg@hotmail.com>
2023-02-20 16:36:10 -0300

    feat:  add TaskObjectMother class

--- backend/tests/model/task/taskObjectMother.mjs
+++ backend/tests/model/task/taskObjectMother.mjs
@@ -2,6 +2,30 @@ import { TaskDataBuilder } from './taskDataBuilder.mjs';
 
 export class TaskObjectMother {
   static valid() {
-    return TaskDataBuilder.validTask();
+    return TaskDataBuilder.validTask().build();
+  }
+
+  static withInvalidName() {
+    return TaskDataBuilder.validTask().withInvalidName().build();
+  }
+
+  static withInvalidUserId() {
+    return TaskDataBuilder.validTask().withInvalidUserId().build();
+  }
+
+  static withoutDescription() {
+    return TaskDataBuilder.validTask().withoutDescription().build();
+  }
+
+  static withoutPriority() {
+    return TaskDataBuilder.validTask().withoutPriority().build();
+  }
+
+  static withoutDone() {
+    return TaskDataBuilder.validTask().withoutDone().build();
+  }
+
+  static withoutDeadline() {
+    return TaskDataBuilder.validTask().withoutDeadline().build();
   }
 }
