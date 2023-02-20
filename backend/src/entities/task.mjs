852f83f5c072f9e4c62470e4ba07f0561f0deaf4
Jean F. dos Santos <jeanddg@hotmail.com>
2023-02-20 16:35:13 -0300

    feat: add new properties

--- backend/src/entities/task.mjs
+++ backend/src/entities/task.mjs
@@ -1,16 +1,20 @@
 export class Task {
   constructor({
+    id = undefined,
     name,
     description = undefined,
     priority = 0,
     deadline,
     done = false,
+    userId = undefined,
   }) {
+    this.id = id;
     this.name = name;
     this.description = description;
     this.priority = priority;
     this.priority = this.priority;
     this.deadline = deadline;
     this.done = done;
+    this.userId = userId;
   }
 }
