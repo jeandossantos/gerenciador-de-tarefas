198bb4adb1831132fa9b402ffb4fe14fa7c6fd5e
Jean F. dos Santos <jeanddg@hotmail.com>
2023-02-23 09:23:32 -0300

    feat: create migrations

---
+++ backend/src/database/migrations/20230222212919_table_users.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.text('name').notNullable();
+    table.text('initials', 2).notNullable();
+    table.text('email').notNullable().unique();
+    table.text('password').notNullable();
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('users');
+}
---
+++ backend/src/database/migrations/20230223122040_table_tasks.js
@@ -0,0 +1,29 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('tasks', (table) => {
+    table.increments('id').primary();
+    table
+      .integer('userId')
+      .notNullable()
+      .references('users.id')
+      .onDelete('CASCADE');
+    table.text('name').notNullable();
+    table.text('description');
+    table.integer('priority', 1).notNullable();
+    table.timestamp('deadline').notNullable();
+    table.boolean('done').defaultTo(false);
+    table.timestamp('createdAt').defaultTo(knex.fn.now());
+    table.timestamp('updatedAt').defaultTo(knex.fn.now());
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('tasks');
+}
