7cf5c6e1db12c79257ed93a73f59b7263e6c3c35
Jean F. dos Santos <jeanddg@hotmail.com>
2023-02-23 10:18:25 -0300

    feat: add create user repository

--- backend/src/repository/userRepository.mjs
+++ backend/src/repository/userRepository.mjs
@@ -27,4 +27,20 @@ export class UserRepository extends AbstractUserRepository {
   async findByEmail(email) {
     return await knex('users').where({ email }).first();
   }
+
+  async create(user) {
+    const tarefaDefault = {
+      name: 'Primeiro Login',
+      description: 'Está é uma tarefa criada por padrão pelo sistema.',
+      priority: 0,
+      deadline: new Date(),
+      done: false,
+    };
+
+    await knex.transaction(async (trx) => {
+      const id = await trx('users').insert(user, 'id');
+
+      await trx('tasks').insert({ ...tarefaDefault, userId: id });
+    });
+  }
 }
