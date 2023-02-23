7770297dd0b446a08d79af7a73ecb9775c20c8cc
Jean F. dos Santos <jeanddg@hotmail.com>
2023-02-23 18:54:52 -0300

    feat: add endpoints

--- backend/src/routes.mjs
+++ backend/src/routes.mjs
@@ -2,8 +2,10 @@ import { Router } from 'express';
 import {
   createUserController,
   authenticateUserController,
+  validateUserTokenController,
 } from './controller/index.mjs';
 
+import { ensureAuthenticated } from './middleware/ensureAuthenticated.mjs';
 const routes = Router();
 
 routes.post('/signin', (req, res) => {
@@ -12,39 +14,24 @@ routes.post('/signin', (req, res) => {
 routes.post('/signup', (req, res) => {
   return createUserController.handle(req, res);
 });
+routes.post('/validatetoken', (req, res) => {
+  return validateUserTokenController.handle(req, res);
+});
+
+routes.delete('/users/:id', ensureAuthenticated, (req, res) => {});
+routes.put('/users/:id', ensureAuthenticated, (req, res) => {});
+routes.put('/users/update/:id', ensureAuthenticated, (req, res) => {});
+
+routes.post('/tasks', ensureAuthenticated, (req, res) => {});
+routes.get('/tasks', ensureAuthenticated, (req, res) => {});
+
+routes.get('/tasks/daily', ensureAuthenticated, (req, res) => {});
+
+routes.put('/tasks/:id', ensureAuthenticated, (req, res) => {});
+routes.delete('/tasks/:id', ensureAuthenticated, (req, res) => {});
+
+routes.post('/tasks/finish/:id', ensureAuthenticated, (req, res) => {});
+
+routes.get('/stats', ensureAuthenticated, (req, res) => {});
 
 export { routes };
-
-// app.post('/validatetoken', authApi.validateToken);
-
-// app.route('/users/:id')
-// .all(passport.authenticate())
-// .delete(userApi.remove)
-// .put(userApi.save)
-
-// app.route('/users/update/:id')
-//     .all(passport.authenticate())
-//     .put(userApi.updatePassword)
-
-// app.route('/tasks')
-//     .all(passport.authenticate())
-//     .post(taskApi.save)
-//     .get(taskApi.getAll)
-
-// app.route('/tasks/daily')
-//     .all(passport.authenticate())
-//     .get(taskApi.getDialyTasks)
-
-// app.route('/tasks/:id')
-//     .all(passport.authenticate())
-//     .put(taskApi.save)
-//     .delete(taskApi.remove)
-
-// app.route('/tasks/finish/:id')
-//     .all(passport.authenticate())
-//     .post(taskApi.finish)
-
-// app.route('/stats')
-//     .all(passport.authenticate())
-//     .get(app.src.api.stats.stats)
-// }
