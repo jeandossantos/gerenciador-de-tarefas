cd3c4958916da9969d5b331d5d0a68bb881998c8
Jean F. dos Santos <jeanddg@hotmail.com>
2023-02-23 09:07:03 -0300

    feat(CreateUserService): add server

---
+++ backend/src/app.mjs
@@ -0,0 +1,40 @@
+import 'express-async-errors';
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import { BaseError } from './errors/base/baseError.mjs';
+import { NotImplementedError } from './errors/notImplementedError.mjs';
+import { Util } from './utils/util.mjs';
+
+import { routes } from './routes.mjs';
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(routes);
+
+app.use((error, req, res, next) => {
+  if (error instanceof NotImplementedError) {
+    console.error(error.message);
+
+    return res
+      .status(Util.STATUS_CODES.Internal_Server_Error)
+      .send('Internal Server Error');
+  }
+
+  if (error instanceof BaseError) {
+    return res.status(Util.STATUS_CODES.Bad_Request).json({
+      error: error.message,
+    });
+  }
+
+  console.error(error.message);
+
+  return res
+    .status(Util.STATUS_CODES.Internal_Server_Error)
+    .send('Internal Server Error');
+});
+
+export { app };
---
+++ backend/src/server.mjs
@@ -0,0 +1,5 @@
+import { app } from './index.mjs';
+
+const PORT = 3001;
+
+app.listen(PORT, () => console.log(`Running server on port ${PORT}`));
