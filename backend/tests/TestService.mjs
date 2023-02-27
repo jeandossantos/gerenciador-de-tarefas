a9350e0517bf5595f8975bfa263a4b6c732a1f81
Jean F. dos Santos <jeanddg@hotmail.com>
2023-02-27 10:56:05 -0300

    feat: add cone to test

--- backend/tests/TestService.mjs
+++ backend/tests/TestService.mjs
@@ -1,43 +1,65 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
-import pg from 'pg';
 import { setTimeout } from 'timers/promises';
-
-import { prisma } from '../src/database/prisma.mjs';
 import { EXISTING_USER } from './mock/existing-user.mjs';
 import { EXISTING_TASKS } from './mock/existing-tasks.mjs';
+
+import { PrismaClient } from '@prisma/client';
+import pg from 'pg';
+
+const prisma = new PrismaClient({
+  log: ['query', 'error'],
+});
+
+const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
 /**this method populate the database with a user and tasks. */
 export async function populateDatabase() {
   await clearDatabase();
 
   await setTimeout(200, 'deleted');
 
-  const user = await prisma.users.create({
-    data: {
-      ...EXISTING_USER,
-      tasks: {
-        createMany: {
-          data: [...EXISTING_TASKS],
+  await prisma.$connect();
+
+  try {
+    const user = await prisma.users.create({
+      data: {
+        ...EXISTING_USER,
+        tasks: {
+          createMany: {
+            data: [...EXISTING_TASKS],
+          },
         },
       },
-    },
-  });
+    });
 
-  const task = await prisma.tasks.findFirst({
-    where: {
-      userId: user.id,
-    },
-  });
+    const task = await prisma.tasks.findFirst({
+      where: {
+        userId: user.id,
+      },
+    });
 
-  return { user, task };
+    return { user, task };
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 /** this method removes the existing user and consequently his tasks.*/
 export async function clearDatabase() {
-  const client = new pg.Pool({
-    max: 100,
-    connectionString: process.env.DATABASE_URL,
-  });
+  try {
+    await client.connect();
 
-  await client.query('DELETE FROM users');
+    await client.query('DELETE FROM users');
+
+    await client.end();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
